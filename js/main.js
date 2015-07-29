4bbccac26e6b7caa89e969c3752e83d0e85f9ef9
Meghan Smith <meghansmithsc@gmail.com>
2015-07-29 17:29:02 -0400

    Add jquery, update styles for medium and large sizes for nav, header, intro, and offices sections.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+$(document).ready(function(){
+	
 
+	$("#menuButton").click(function(){
+		$(".menuHide").slideToggle('fast');
+		
+	});
+
+
+});
---
+++ js/min/main-min.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$("#menuButton").click(function(){$(".menuHide").slideToggle("fast")})});
