8075ecffe1d16149ad1e1ffd51bae45521d91ec8
Meghan Smith <meghansmithsc@gmail.com>
2015-07-31 13:59:57 -0400

    add smooth scrolling

--- js/min/main-min.js
+++ js/min/main-min.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$("#menuButton").click(function(){$(".menuHide").slideToggle("fast")})});
+$(document).ready(function(){$("#menuButton").click(function(){$(".menuHide").slideToggle("fast")}),$("a").click(function(){return $("html, body").animate({scrollTop:$($.attr(this,"href")).offset().top},500),!1})});
